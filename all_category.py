0e6906247bbb9d13f028652e17fac183b2947ea4
spaunder76 <arthurbousquet36@gmail.com>
2023-02-16 15:02:14 +0100

    ajout final de tous les fichiers

--- all_category.py
+++ all_category.py
@@ -4,16 +4,32 @@ from urllib.parse import urljoin
 from category import info_category
 import csv
 
-# URL of the Homepage
-home_url = 'http://books.toscrape.com/index.html'
+def all_category():
+    # URL of the Homepage
+    home_url = 'http://books.toscrape.com/index.html'
 
-# Do a request on the homepage to get the links of the category
-response = requests.get(home_url)
-soup = BeautifulSoup(response.text, 'lxml')
-categories = soup.find_all('h3')
+    # Do a request on the homepage to get the links of the category
+    response = requests.get(home_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    categories = soup.find("div", {"class": "side_categories"})
+    print('categories', categories)
 
-# for each category, get info of the books
-for category in categories:
-    category_url = urljoin(home_url, category.find('a')['href'])
-    info_category(category_url)
-    
+    # BODY <---- soup
+    # -----| DIV class="side_categories" <---- categories ######
+    # ------------| a href="categoryX.html" <----book_categories
+    # ------------| span titre
+    # ------------| a href="categoryY.html" <----book_categories
+    # ------------| a href="categoryZ.html" <----book_categories
+
+    book_categories = categories.find_all('a')
+    print('book_categories', book_categories)
+
+
+    # for each category, get info of the books
+    for category in book_categories:
+        category_url = urljoin(home_url, category.find('a')['href'])
+        category_name = category.find('a')['text']
+        print('Category name: ', category_name)
+        info_category(category_url, category_name)
+        
+all_category()
--- category.py
+++ category.py
@@ -5,30 +5,25 @@ from books import info_books
 import csv
 import time
 
-def info_category(url):
-    # do a request on the page to get the book informations
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    books = soup.find_all('h3')
+def info_category(url, category_name):
+    with open('categories/' + category_name + '.csv', 'w', newline='') as csvfile:
+        csvWriter = csv.writer(csvfile, delimiter=';')
+        while True:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "lxml")
+            books = soup.find_all('h3')
 
-    # For every books, get the informations
-    for book in books:
-        book_url = urljoin(url, book.find('a')['href'])
-        info_books(book_url)
+            for book in books:
+                book_url = urljoin(url, book.find('a')['href'])
+                info_books(book_url, csvWriter)
 
-    while True:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "lxml")
-        footer_element = soup.select_one('li.current')
-        # Do more with each page.
+            next_page_element = soup.select_one('li.next > a')
 
-        # Find the next page to scrape in the pagination.
-        next_page_element = soup.select_one('li.next > a')
-        if next_page_element:
-            next_page_url = next_page_element.get('href')
-            url = urljoin(url, next_page_url)
-            info_category(url)
-        else:
-            break
-url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
-info_category(url)
+            if next_page_element:  
+                next_page_url = next_page_element.get('href')
+                url = urljoin(url, next_page_url)
+            else:
+                break
+
+url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+info_category(url, 'mystery')
